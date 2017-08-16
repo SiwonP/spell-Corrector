727edf252c152f881010de6be8effd94f52957eb
Simon Petit <nomisp96@hotmail.fr>
2017-08-16 19:49:32 +0200

    keep going

--- treatment.c
+++ treatment.c
@@ -7,6 +7,14 @@ char **splitWordsFromReferenceText(FILE *file)
     char **splits;
     splits = malloc(200 * sizeof(char*));
 
+    char word[50];
+
+    char c;
+    c = fgetc(file);
+    
+    while(c != EOF) {
+
+    }
 
     return splits;
 }
