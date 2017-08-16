8e88710ae6a32720ca5f4339dd5c5d5665e3d69e
Simon Petit <nomisp96@hotmail.fr>
2017-08-16 19:06:37 +0200

    going on

---
+++ treatment.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "treatment.h"
+
+char **splitWordsFromReferenceText(FILE *file)
+{
+    char **splits;
+    splits = malloc(200 * sizeof(char*));
+
+
+    return splits;
+}
---
+++ treatment.h
@@ -0,0 +1,8 @@
+#ifndef TREATMENT_H
+#define TREATMENT_H
+
+#include <stdio.h>
+
+char **splitWordsFromReferenceText(FILE*);
+
+#endif
